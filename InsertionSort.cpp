39160acd42aeff0f96cc06f72d225c10ae1ef0fd
alivas <qingyun.oracle@gmail.com>
2014-03-10 23:45:32 -0700

    Add InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include "RandomArray.h"
+
+using namespace std;
+
+void InsertionSort(int A[], int size)
+{
+    // loop from 1 to size -1
+    for (int j = 1; j < size; ++j)
+    {
+        int key = A[j];
+        int i = j - 1;
+
+        while (i >= 0 && A[i] > key)
+        {
+            // !! assign A[i] to A[i+1]
+            A[i + 1] = A[i];
+            i--;
+        }
+
+        // A[i+1] is the last member who is bigger than key and moved afterward.
+        // Or A[i+1] is A[0].
+        A[i + 1] = key;
+    }
+}
+
+#ifndef MAIN
+int main()
+{
+    int A[LENGTH] = {0};
+    initializeArray(A, LENGTH);
+    printArray(A, LENGTH);
+    InsertionSort(A, LENGTH);
+    printArray(A, LENGTH);
+    return 0;
+}
+#endif
